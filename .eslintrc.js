aa9c422a4632be162de51c8614cd40e356bc5b0f
Guillaume Peyssard <guillaume.peyssard@orange.fr>
2023-01-01 12:50:12 +0900

    Upgrade packages and install linter

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+// Configuration module for eslintrc,
+// which is a js code (and more) linter.
+// eslint is also used to format the code with the vscode ESLint plugin.
+// To activate format on save, add "eslint.validate": ["javascript"]" to the .vscode/settings.json file.
+
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
+    extends: [
+      // add more generic rulesets here, such as:
+      // 'eslint:recommended',
+      // 'plugin:vue/vue3-recommended',
+      'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    ],
+    rules: {
+      "indent": ["error", 2],
+      "no-console": "off",
+      "no-debugger": "error",
+      "semi": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "space-before-function-paren": ["error", {
+        "anonymous": "always",
+        "named": "never",
+        "asyncArrow": "always",
+      }],
+      "quotes": [
+        "error",
+        "double",
+      ],
+      "vue/html-closing-bracket-newline": ["error", {
+        "singleline": "never",
+        "multiline": "never",
+      }],
+      "vue/max-attributes-per-line": ["error", {
+        "singleline": 4,
+        "multiline": 1,
+      }],
+      "vue/attribute-hyphenation": "off",
+      "vue/html-closing-bracket-spacing": ["error", {
+        "startTag": "never",
+        "endTag": "never",
+        "selfClosingTag": "always",
+      }],
+      "vue/multi-word-component-names": "off",
+      "vue/component-name-in-template-casing": ["error", "PascalCase"],
+      "import/no-named-as-default": 0,
+      "no-unused-vars": "warn",
+    },
+  };
+  
--- src/views/Admin/Clients/Add.vue
+++ src/views/Admin/Clients/Add.vue
@@ -6,31 +6,35 @@
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
           <div class="page-header">
             <div class="d-flex justify-content-between">
-              <h2 class="pageheader-title">Clients</h2>
+              <h2 class="pageheader-title">
+                Clients
+              </h2>
             </div>
             <div class="page-breadcrumb">
               <nav aria-label="breadcrumb">
                 <ol class="breadcrumb d-flex align-items-center">
                   <li class="breadcrumb-item">
-                    <router-link :to="{ name: 'home' }" class="breadcrumb-link"
-                      >Dashboard</router-link
-                    >
+                    <router-link
+                      :to="{ name: 'home' }"
+                      class="breadcrumb-link">
+                      Dashboard
+                    </router-link>
                   </li>
                   <font-awesome-icon
                     icon="angle-right"
                     size="xs"
-                    class="mx-1"
-                  />
+                    class="mx-1" />
                   <li class="breadcrumb-item">
-                    <router-link to="/clients" class="breadcrumb-link"
-                      >Clients</router-link
-                    >
+                    <router-link
+                      to="/clients"
+                      class="breadcrumb-link">
+                      Clients
+                    </router-link>
                   </li>
                   <font-awesome-icon
                     icon="angle-right"
                     size="xs"
-                    class="mx-1"
-                  />
+                    class="mx-1" />
                   <li class="breadcrumb-item active" aria-current="page">
                     Ajouter un client
                   </li>
@@ -42,61 +46,69 @@
       </div>
 
       <Alert
+        v-if="errors.visible"
         :type="errors.type"
         :message="errors.message"
-        v-if="errors.visible"
-        @dismiss="errors.visible = false"
-      />
+        @dismiss="errors.visible = false" />
 
       <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
           <div class="card">
             <div
-              class="card-header d-flex align-items-center justify-content-between"
-            >
-              <h5 class="mb-0">Ajouter un client</h5>
+              class="card-header d-flex align-items-center justify-content-between">
+              <h5 class="mb-0">
+                Ajouter un client
+              </h5>
             </div>
             <div class="card-body">
-              <h4 class="mb-0">Nouveau client</h4>
+              <h4 class="mb-0">
+                Nouveau client
+              </h4>
               <p>Le compte lié au nouveau terminal.</p>
               <form>
                 <div class="row">
                   <div class="form-group col">
-                    <label for="company" class="col-form-label"
-                      >Nom de l'entreprise</label
-                    >
+                    <label
+                      for="company"
+                      class="col-form-label">Nom de l'entreprise</label>
                     <input
                       v-model="customer.company"
                       type="text"
-                      class="form-control"
-                    />
+                      class="form-control">
                   </div>
                   <div class="form-group col">
-                    <label for="inputEmail" class="col-form-label"
-                      >Contact au sein de l'entreprise</label
-                    >
+                    <label
+                      for="inputEmail"
+                      class="col-form-label">Contact au sein de l'entreprise</label>
                     <input
+                      v-model="customer.representative"
                       type="email"
                       class="form-control"
-                      placeholder="name@example.com"
-                      v-model="customer.representative"
-                    />
+                      placeholder="name@example.com">
                   </div>
                 </div>
                 <div class="row">
                   <div class="form-group col-12">
                     <label for="sales_type">Type de vente</label>
-                    <select class="custom-select" v-model="customer.sales_type">
-                      <option value="A">Achat</option>
-                      <option value="S">Abonnement</option>
-                      <option value="L">Location</option>
+                    <select v-model="customer.sales_type" class="custom-select">
+                      <option value="A">
+                        Achat
+                      </option>
+                      <option value="S">
+                        Abonnement
+                      </option>
+                      <option value="L">
+                        Location
+                      </option>
                     </select>
                   </div>
                 </div>
               </form>
             </div>
             <div class="card-body border-top">
-              <h4 class="mb-0">Médias</h4>
+              <h4 class="mb-0">
+                Médias
+              </h4>
               <p>
                 Il est important de compresser les images au maximum avant de
                 les uploader.
@@ -106,16 +118,15 @@
                   <div class="form-group col">
                     <label for="logo">Logo</label>
                     <input
+                      ref="logo"
                       type="file"
                       class="form-control-file"
                       aria-describedby="fileHelpId"
-                      name="logo"
-                      ref="logo"
-                    />
-                    <small id="fileHelpId" class="form-text text-muted"
-                      >Logo PNG, JPEG ou SVG permettant d'identifier clairement
-                      l'entreprise.</small
-                    >
+                      name="logo">
+                    <small
+                      id="fileHelpId"
+                      class="form-text text-muted">Logo PNG, JPEG ou SVG permettant d'identifier clairement
+                      l'entreprise.</small>
                   </div>
                 </div>
               </form>
@@ -135,7 +146,7 @@
 <script>
 export default {
   name: "AddClient",
-  data: function() {
+  data: function () {
     return {
       customer: {},
       errors: {
@@ -145,9 +156,9 @@ export default {
       },
     };
   },
-  mounted: function() {},
+  mounted: function () {},
   methods: {
-    addClient: function() {
+    addClient: function () {
       const form = new FormData();
       form.append("company", this.customer.company);
       form.append("representative", this.customer.representative);
